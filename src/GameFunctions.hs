fcb2c82bf6f467270b67c074608eb1a79ea23d3e
mttgermano <mttgermano@proton.me>
2024-02-27 11:32:44 -0300

    bug fix pt3

--- src/GameFunctions.hs
+++ src/GameFunctions.hs
@@ -1,4 +1,8 @@
 module GameFunctions where
+import DbFunctions
+
+import GHC.Generics
+import Data.Aeson
 
 import qualified Data.ByteString.Lazy.Char8 as BS1
 import qualified Data.ByteString.Char8 as BS2
@@ -9,18 +13,43 @@ import Database.PostgreSQL.Simple.ToRow
 import Database.PostgreSQL.Simple.Types (Query(Query))
 
 
+-- User Data Type
+data User = User
+    { uId :: String,
+      uName :: String,
+      uPassword :: String
+    }
+    deriving (Show, Generic)
+
+-- Automatically derive FromJSON instance
+instance FromJSON User
+
+-- Convert User into a tuple for SQL insert
+instance ToRow User where
+    toRow user = [toField (uId user), toField (uName user), toField (uPassword user)]
+
+
 -- Increment the vote for a user in the db
-incrementVote :: String -> IO ()
-incrementVote userName = do
-    conn <- getDbConnection
+incrementVote :: BS1.ByteString -> IO ()
+incrementVote jsonRequest = do
+    let maybeUser = parseUserRequest jsonRequest
 
-    -- DB Query ----------------------------------
-    [Only user_id] <- query conn "SELECT user_id FROM users WHERE user_name = ?" (Only userName)
-    execute conn "UPDATE votes SET number_of_votes = number_of_votes + 1 WHERE user_id = ?" (Only (user_id :: Int))
-    ----------------------------------------------
-    close conn
-    putStrLn $ "Vote incremented for user " ++ userName
+    case maybeUser of
+        Just user -> do
+            conn <- getDbConnection
 
--- Establish a database connection
-getDbConnection :: IO Connection
-getDbConnection = connectPostgreSQL $ BS2.pack "host=localhost dbname=mydatabase user=myuser password=mypassword"
+            -- DB Query ----------------------------------
+            let sqlQuery = Query $ BS2.pack "UPDATE votes SET number_of_votes = number_of_votes + 1 WHERE user_id = ?"
+            result <- execute conn sqlQuery (uName user, uPassword user)
+            ----------------------------------------------
+            close conn
+
+            putStrLn $ "Vote incremented for user " ++ (uName user)
+
+        Nothing ->
+            putStrLn "Invalid JSON format."
+
+
+-- Parse a POST request
+parseUserRequest :: BS1.ByteString -> Maybe User
+parseUserRequest = decode
