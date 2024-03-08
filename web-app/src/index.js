6f17aa3e80bece781a03d9c0a66959cf33c4fb6f
mttgermano <mttgermano@proton.me>
2024-03-08 13:12:34 -0300

    file paths

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import './css/index.css';
+import App from './pages/App';
+import reportWebVitals from './components/reportWebVitals';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
