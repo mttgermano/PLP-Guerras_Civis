1bd943a5a149a8dd21772ceb87d50418deec2895
Gabrielhlc <gabrielhenlc@gmail.com>
2024-03-24 16:23:55 -0300

    pages: createRoom, RoomHome, RoomPage

--- web-app/src/pages/App.jsx
+++ web-app/src/pages/App.jsx
@@ -6,6 +6,8 @@ import MainPage from './MainPage.jsx';
 import LoginPage from './LoginPage.jsx';
 import RegistPage from './RegistPage.jsx';
 import RoomPage from './RoomPage.jsx';
+import CreateRoom from './CreateRoom.jsx'
+import RoomHome from './RoomHome.jsx'
 import { UserContextProvider } from '../contexts/userContext.jsx';
 
 function App() {
@@ -21,8 +23,9 @@ function App() {
             <Route path="/login/login_player" element={<LoginPage />} />
             <Route path="/login/create_player" element={<RegistPage />} />
             <Route path="/room/login_room" element={<LoginPage />} />
-            <Route path="/room/create_room" element={<RegistPage />} />
-            <Route path="/room/home" element={<RoomPage />} />
+            <Route path="/room/home" element={<RoomHome />} />
+            <Route path="/room/create_room" element={<CreateRoom />} />
+            <Route path="/room/game" element={<RoomPage />} />
           </Routes>
         </UserContextProvider>
       </div>
