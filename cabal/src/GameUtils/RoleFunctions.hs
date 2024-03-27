72c00d975213ab4b1a753b14bcb4ca4ff1b10961
mttgermano <mttgermano@proton.me>
2024-03-27 01:10:39 -0300

    update role functions [You can now authenticate]

--- cabal/src/GameUtils/RoleFunctions.hs
+++ cabal/src/GameUtils/RoleFunctions.hs
@@ -36,55 +36,98 @@ kill agent action_reciever = do
 -- Save a player of being killed
 save :: String -> String -> IO ()
 save agent action_reciever = do
-    conn <- getDbConnection
+    allowed <- isAllowed agent
 
+    if allowed
+        then do
+            conn    <- getDbConnection
     
-    -- DB Query ----------------------------------
-    let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET kill_vote = kill_vote -1 WHERE user_id = ?"
-    result <- execute conn sqlQuery (Only agent)
-    ----------------------------------------------
-    putStrLn $ ("> User [" ++ agent ++ "] Saved [" ++ (action_reciever) ++ "]")
-    close conn
+            -- DB Query ----------------------------------
+            let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET kill_vote = kill_vote -1 WHERE user_id = ?"
+            result <- execute conn sqlQuery (Only agent)
+            ----------------------------------------------
+            putStrLn $ ("> User [" ++ agent ++ "] Saved [" ++ (action_reciever) ++ "]")
+            close conn
+        else
+            errPermissionMessage agent
 
 
 -- See the identity of a player
 search :: String -> String -> IO ()
 search agent action_reciever = do
-    putStrLn $ ("> User [" ++ agent ++ "] Searched Role for [" ++ (action_reciever) ++ "]")
+    allowed <- isAllowed agent
+
+    if allowed
+        then do
+            conn    <- getDbConnection
+            putStrLn $ ("> User [" ++ agent ++ "] Searched Role for [" ++ (action_reciever) ++ "]")
+        else
+            errPermissionMessage agent
 
 -- Reveal the indetity of a player to all the other ones
 reveal :: String -> String -> IO ()
 reveal agent action_reciever = do
-    putStrLn $ ("> User [" ++ agent ++ "] Revealed [" ++ (action_reciever) ++ "]")
+    allowed <- isAllowed agent
+
+    if allowed
+        then do
+            conn    <- getDbConnection
+            putStrLn $ ("> User [" ++ agent ++ "] Revealed [" ++ (action_reciever) ++ "]")
+        else
+            errPermissionMessage agent
 
 
 -- Silence a player
 silence :: String -> String -> IO ()
 silence agent action_reciever = do
-    conn <- getDbConnection
+    allowed <- isAllowed agent
 
-    -- DB Query ----------------------------------
-    let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET is_silenced = true WHERE user_id = ?"
-    result <- execute conn sqlQuery (Only agent)
-    ----------------------------------------------
-    putStrLn $ ("> User [" ++ agent ++ "] Silenced [" ++ (action_reciever) ++ "]")
-    close conn
+    if allowed
+        then do
+            conn    <- getDbConnection
+
+            -- DB Query ----------------------------------
+            let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET is_silenced = true WHERE user_id = ?"
+            result <- execute conn sqlQuery (Only agent)
+            ----------------------------------------------
+            putStrLn $ ("> User [" ++ agent ++ "] Silenced [" ++ (action_reciever) ++ "]")
+            close conn
+        else
+            errPermissionMessage agent
 
 
 -- Paralize a player
 paralize :: String -> String -> IO ()
 paralize agent action_reciever = do
-    conn <- getDbConnection
+    allowed <- isAllowed agent
 
-    -- DB Query ----------------------------------
-    let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET is_paralized = true WHERE user_id = ?"
-    result <- execute conn sqlQuery (Only agent)
-    ----------------------------------------------
-    putStrLn $ ("> User [" ++ agent ++ "] Paralized [" ++ (action_reciever) ++ "]")
-    close conn
+    if allowed
+        then do
+            conn    <- getDbConnection
+
+            -- DB Query ----------------------------------
+            let sqlQuery = Query $ BS2.pack "UPDATE UserGameData SET is_paralized = true WHERE user_id = ?"
+            result <- execute conn sqlQuery (Only agent)
+            ----------------------------------------------
+            putStrLn $ ("> User [" ++ agent ++ "] Paralized [" ++ (action_reciever) ++ "]")
+            close conn
+        else
+            errPermissionMessage agent
 
 
 -- Set the Cursed word for a Room
 setCursedWord :: String -> String -> IO ()
-setCursedWord agent action_reciever = do
-    putStrLn $ ("> User [" ++ agent ++ "] set Cursed Word for [" ++ "] rName")
+setCursedWord agent cursedWord = do
+    allowed <- isAllowed agent
+
+    if allowed
+        then do
+            conn    <- getDbConnection
+
+            -- DB Query ----------------------------------
+            let sqlQuery = Query $ BS2.pack "UPDATE Room SET cursed_word = ? WHERE room_uuid = (SELECT current_room FROM Player WHERE player_name = ?)"
+            result <- execute conn sqlQuery (cursedWord, agent)
+            ----------------------------------------------
+            putStrLn $ ("> User [" ++ agent ++ "] set Cursed Word for [" ++ "] rName")
+        else
+            errPermissionMessage agent
