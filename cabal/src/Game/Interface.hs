965a5659deee1e3d1b31a1d7bc67b4729741b881
davi-sf <davi.soares.ferreira@ccc.ufcg.edu.br>
2024-04-01 13:08:37 -0300

    feat: criar usario / loggar na interface

--- cabal/src/Game/Interface.hs
+++ cabal/src/Game/Interface.hs
@@ -46,4 +46,61 @@ drawInterface = do
     drawVerticalLine 84 "[2] Loggar" 14
     drawHorizontalLine 84
     setSGR [Reset]
+-- Function to create a user with validation
+createUser :: String -> IO ()
+createUser nome = do
+    putStrLn "Insira a senha:"
+    senha1 <- getLine
+    putStrLn "Confirme a senha:"
+    senha2 <- getLine
+    if senha1 == senha2
+        then putStrLn $ "Usuário '" ++ nome ++ "' criado com sucesso"
+        else do
+            putStrLn "Senhas devem conferir, tente novamente."
+            createUser nome
+
+
+-- Function to draw the create user interface
+drawCreateUserInterface :: IO ()
+drawCreateUserInterface = do
+    clear
+    setSGR [SetColor Foreground Vivid White]
+    drawHorizontalLine 84
+    drawVerticalLine 84 "Criar usuario" 32
+    drawHorizontalLine 84
+    putStrLn "|                                                                               |"
+    putStrLn "|                                                                               |"
+    putStrLn "|                                                                               |"
+    drawVerticalLine 84 "Usuario:" 10
+    drawVerticalLine 84 "Senha:" 10
+    drawVerticalLine 84 "Confirme a senha:" 10
+    drawHorizontalLine 84
+    setSGR [Reset]
+
+
+-- Function to login an user
+loginUser :: IO ()
+loginUser = do
+    drawLoginInterface
+    putStrLn "Insira o nome:"
+    username <- getLine
+    putStrLn "Insira a senha:"
+    password <- getLine
+    --  So exibe 'Usario criado com sucesso' - falta integrar o login
+    putStrLn $ "Logado com'" ++ username ++ "'"
+
+-- Function to draw the login interface
+drawLoginInterface :: IO ()
+drawLoginInterface = do
+    clear
+    setSGR [SetColor Foreground Vivid White]
+    drawHorizontalLine 84
+    drawVerticalLine 84 "Loggar" 32
+    drawHorizontalLine 84
+    putStrLn "|                                                                               |"
+    drawVerticalLine 84 "Username:" 10
+    drawVerticalLine 84 "Password:" 10
+    drawHorizontalLine 84
+    setSGR [Reset]
+
 
