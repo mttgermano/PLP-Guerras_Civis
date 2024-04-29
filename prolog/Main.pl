7a7212b5e9a2bbcd23b39620d2eafa0abaa20931
mttgermano <mttgermano@proton.me>
2024-04-29 13:51:11 -0300

    refactoring pt2

--- prolog/Main.pl
+++ prolog/Main.pl
@@ -1,5 +1,12 @@
 :- initialization main, halt.
+
 :- include('Controllers/Menu.pl').
+:- include('Login/Player.pl').
+:- include('Database/Players.pl').
+:- include('Database/PlayersKnowledge.pl').
+:- include('Database/UserGameData.pl').
+% :- include('Game/BotLogic.pl').
+% :- include('Game/RoleFunctions.pl').
 
 main :-
     writeln("[Main is working]"),
