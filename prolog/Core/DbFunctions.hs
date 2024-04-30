441937b9ce5d82901b907c8de4fbad54ae7af4cb
mttgermano <mttgermano@proton.me>
2024-04-30 16:44:06 -0300

    delete unused

--- prolog/Core/DbFunctions.hs
+++
@@ -1,8 +0,0 @@
-module Core.DbFunctions where
-
-import qualified Data.ByteString.Char8 as BS2
-import Database.PostgreSQL.Simple
-
--- Establish a database connection
-getDbConnection :: IO Connection
-getDbConnection = connectPostgreSQL $ BS2.pack "host=localhost dbname=plp_db user=root password=plp123"
