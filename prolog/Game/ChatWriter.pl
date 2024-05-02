90f10c5723e265adc76f24804e41945d9e5dd306
Mr-76 <vitor.holanda@ccc.ufcg.edu.br>
2024-05-01 22:32:37 -0400

    exit ok

--- prolog/Game/ChatWriter.pl
+++ prolog/Game/ChatWriter.pl
@@ -5,7 +5,7 @@
 menu_template("WriteChat",
     [
     "┌───────────────────────────── Guerras Civis ──────────────────────────────┐",
-    "│Write your mensage                                                        │",
+    "│Write your mensage,write exit to stop.                                    │",
     "│                                                                          │",
     "└──────────────────────────────────────────────────────────────────────────┘"]).
 
@@ -15,18 +15,23 @@ write_chat(MenuTemplate) :-
     cl,
     print_menu(MenuTemplate),
     read_line_to_string(user_input,Input),
-    writeOut(Input),
+    write_to_chat_till_exit,
     menu_template("Start", Menu),
     menu_main(Menu).
 
+write_to_chat_till_exit :-
+    write_word(Word),( Word = "exit" ->  true;   write_to_chat_till_exit).
 
+write_word(Word) :-
+    read_line_to_string(user_input, Line),
+    split_string(Line, " ", "", [Word|_]),
+    ( Word = "exit" -> true; append_to_file('chat.txt',Line)).
 
+append_newline(String, StringWithNewline) :-
+    atom_concat(String, '.\n', StringWithNewline).
 
-
-
-
-
-
-
-
-
+append_to_file(File,Text) :-
+     append_newline(Text,LineF),
+     open(File, append ,Stream),
+     write(Stream,LineF),nl,
+     close(Stream).
