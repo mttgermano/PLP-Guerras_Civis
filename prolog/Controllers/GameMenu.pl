8f9d25136e36179238d994f2cb412ab2092fd161
Mr-76 <vitor.holanda@ccc.ufcg.edu.br>
2024-05-03 21:22:10 -0400

    some tiny ui fix

--- prolog/Controllers/GameMenu.pl
+++ prolog/Controllers/GameMenu.pl
@@ -1,12 +1,15 @@
+:- include('./Utils.pl').
 menu_template("Game", Rname, Players, IsAlive, Role, Round, State, Menu):-
-    format(string(RoomData),
-        '| > Room: ~w\n|\n| > Round: ~w - ~w\n|',
-        [Rname, Round, State]),
+
+    format(string(RoomData), '│ > Room: ~w                                                             │\n│                                                                          │\n│ > Round: ~w - ~w                                                 │\n│                                                                          │ ', [Rname, Round, State]),
+
+
+
     with_output_to(string(PlayerData), print_lists(Players, IsAlive, Role)),
     Menu = [
             "┌───────────────────────────── Guerras Civis ──────────────────────────────┐",
             RoomData,
-            "| Players:  IsAlive:  Role:                                                |",
+            "│ Players:  IsAlive:  Role:                                                │",
             PlayerData,
             "│                                                                          │",
             "│                                                                          │",
@@ -18,10 +21,11 @@ menu_template("Game", Rname, Players, IsAlive, Role, Round, State, Menu):-
             "│ [2] Enviar Mensagem                                                      │",
             "│                                                                          │",
             "└──────────────────────────────────────────────────────────────────────────┘"].
+
 % Util ---------------------------------------------
 print_lists([], [], []):- !.
 print_lists([Player|Players], [IsAlive|IsAliveList], [Role|Roles]) :-
-    format(atom(PlayerData), "| ~w    ~w         ~w", [Player, IsAlive, Role]),
+    format(atom(PlayerData), "│ ~w    ~w         ~w", [Player, IsAlive, Role]),
     writeln(PlayerData),
     print_lists(Players, IsAliveList, Roles).
 
